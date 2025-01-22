5415f2954d5942d3c8ca04136385b2639ccd91ef
Carlos Astorga <databook.one@gmail.com>
2025-01-22 12:20:18 +0100

    mod script

--- mygitpull_irc.sh
+++ mygitpull_irc.sh
@@ -8,13 +8,16 @@ ACTUAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git status
 git checkout main
 git pull
+echo ".............................."
 git checkout apardo
 git pull
+echo ".............................."
 git checkout castorga
 git pull
+echo ".............................."
 git checkout linyao
 git pull
-
+echo ".............................."
 git checkout $ACTUAL_BRANCH
 
 git status
