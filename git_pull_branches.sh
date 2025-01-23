57f29532f69c2aab1ccd53296333337b7beff558
Carlos Astorga <databook.one@gmail.com>
2025-01-23 11:18:07 +0100

    test

---
+++ git_pull_branches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+IRC_FOLDER=/home/castorga/my_repos/irc-server
+
+cd $IRC_FOLDER
+
+ACTUAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git status
+echo ".............................."
+git checkout main
+git pull
+echo ".............................."
+git checkout apardo
+git pull
+echo ".............................."
+git checkout linyao
+git pull
+echo ".............................."
+git checkout $ACTUAL_BRANCH
