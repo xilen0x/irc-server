9f3db01a424ded3453c21065ad9b22a27e9cc20a
apardo-m <apardo-m@student.42barcelona.com>
2025-01-24 18:43:38 +0100

    deleted apardoś headers in files

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,15 +1,14 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
-#include <iostream>
-#include <vector>
-#include <poll.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <cstring>
-#include <unistd.h>
-#include <cstdlib>
-#include "Client.hpp"
+# include <iostream>
+# include <vector>
+# include <poll.h>
+# include <fcntl.h>
+# include <netinet/in.h>
+# include <cstring>
+# include <unistd.h>
+# include <cstdlib>
 
 # include "Client.hpp"
 # include "Channel.hpp"
